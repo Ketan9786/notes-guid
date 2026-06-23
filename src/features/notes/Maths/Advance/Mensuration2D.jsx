8f59f31c25d5d85f6c4ed98f1739db7072a7d374
ketan1997 <ketan.kulkarni@finmeadows.com>
2026-06-23 19:45:13 +0530

    MENSURATION_TWO_ADDED

--- src/features/notes/Maths/Advance/Mensuration2D.jsx
+++ src/features/notes/Maths/Advance/Mensuration2D.jsx
@@ -1,5 +1,5 @@
 import RevisionViewer from "../RevesionViewer";
-import Menusration2Ddata from "../../../../data/maths/airthmetic/percentage.json";
+import Menusration2Ddata from "../../../../data/maths/advance/mensuration2d.json";
 const Menusration2D = () => {
   return (
     <RevisionViewer chapterData={Menusration2Ddata} />
